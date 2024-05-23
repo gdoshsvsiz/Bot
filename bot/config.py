7425a2d853b879b7f8b5a0d2a67c9eb68a2a6152
gdoshsvsiz <hdoshsgiskdbekeo@outlook.com>
2024-05-23 17:06:31 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,13 +4,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = 6620972
     DEV = 1664850827
-    OWNER = config("OWNER")
+    OWNER = config("OWNER", "6047510747")
+    API_HASH = "3f6835286b03e000ab6d71b37cc35b92"
+    BOT_TOKEN = config("BOT_TOKEN", "6851507284:AAEWTGWXsr-fL4aW5ti3BpC2DLcXnuC-S18")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
+    THUMBNAIL = "https://telegra.ph/file/e67c2bb851ee37be37b5f.jpg"
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
